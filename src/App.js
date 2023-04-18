9e27693087937b39977e9f12514569f049eddd4d
felipemaifredo <felipemaifredo@gmail.com>
2023-04-18 02:14:34 -0300

    att

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home />}/>
+          <Route path='/home' element={<Home />}/>
         </Routes>
       </Router>
     </div>
