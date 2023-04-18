bd128aa622d1585962e39dc8e33379c7ce6dccb6
felipemaifredo <felipemaifredo@gmail.com>
2023-04-18 02:26:11 -0300

    v att hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
 
 //Components
 import Navbar from './components/Navbar/Navbar';
@@ -13,11 +13,11 @@ function App() {
     <div className="App">
       <Navbar />
       <WhatsAppIcon />
-      <Router>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<Home />}/>
         </Routes>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
