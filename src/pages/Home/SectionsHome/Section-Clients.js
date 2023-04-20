f445d76208bde4d2f3735ecae12f8a58236f6747
felipemaifredo <felipemaifredo@gmail.com>
2023-04-20 00:37:39 -0300

    att bug correct js

--- src/pages/Home/SectionsHome/Section-Clients.js
+++ src/pages/Home/SectionsHome/Section-Clients.js
@@ -3,46 +3,47 @@ import './style-section-clients.css';
 //Assets
 import person1 from '../../../assets/persons/1.png'
 //Imports
+import { useState, useRef } from 'react';
 import CardClient from '../../../components/Client-Card/Client-Card';
 import { BsChevronCompactLeft } from "react-icons/bs";
 
+
 const ClientsSection = () => {
-    const productContainers = [...document.querySelectorAll('.container')];
-    const nxtBtn = [...document.querySelectorAll('.Navigation-btn-right')];
-    const preBtn = [...document.querySelectorAll('.Navigation-btn-left')];
+  const containerRef = useRef(null);
+  const [containerWidth, setContainerWidth] = useState(0);
 
-    productContainers.forEach((item, i) => {
-        let containerDimensions = item.getBoundingClientRect();
-        let containerWidth = containerDimensions.width;
+  const handleNextClick = () => {
+    containerRef.current.scrollLeft += containerWidth;
+  };
 
-        nxtBtn[i].addEventListener('click', () => {
-            item.scrollLeft += containerWidth;
-        })
+  const handlePrevClick = () => {
+    containerRef.current.scrollLeft -= containerWidth;
+  };
 
-        preBtn[i].addEventListener('click', () => {
-        item.scrollLeft -= containerWidth;
-        })
-    })
+  const handleContainerResize = () => {
+    const containerDimensions = containerRef.current.getBoundingClientRect();
+    setContainerWidth(containerDimensions.width);
+  };
 
-    return (
-        <section id='ClientsSection' className='section'>
-
-            <button className='Navigation-btn-left'> <BsChevronCompactLeft className='btn-icon-navigation' /> </button>
-
-            <button className='Navigation-btn-right'> <BsChevronCompactLeft className='btn-icon-navigation' /> </button>
-
-            <div className='container'>
-                <CardClient photo={person1} name={'Celso Lopes'} text={'A Help veio literalmente para dar um help nota 10!'} />
-                <CardClient photo={person1} name={'Celso Lopes'} text={'A Help veio literalmente para dar um help nota 10!'} />
-                <CardClient photo={person1} name={'Celso Lopes'} text={'A Help veio literalmente para dar um help nota 10!'} />
-                <CardClient photo={person1} name={'Celso Lopes'} text={'A Help veio literalmente para dar um help nota 10!'} />
-                <CardClient photo={person1} name={'Celso Lopes'} text={'A Help veio literalmente para dar um help nota 10!'} />
-                <CardClient photo={person1} name={'Celso Lopes'} text={'A Help veio literalmente para dar um help nota 10!'} />
-                <CardClient photo={person1} name={'Celso Lopes'} text={'A Help veio literalmente para dar um help nota 10!'} />
-            </div>
-          
-        </section>
-    )
-}
+  return (
+    <section id='ClientsSection' className='section'>
+      <button className='Navigation-btn-left' onClick={handlePrevClick}>
+        <BsChevronCompactLeft className='btn-icon-navigation' />
+      </button>
+      <button className='Navigation-btn-right' onClick={handleNextClick}>
+        <BsChevronCompactLeft className='btn-icon-navigation' />
+      </button>
+      <div className='container' ref={containerRef} onLoad={handleContainerResize} onResize={handleContainerResize}>
+        <CardClient photo={person1} name={'Celso Lopes'} text={'A Help veio literalmente para dar um help nota 10!'} />
+        <CardClient photo={person1} name={'Celso Lopes'} text={'A Help veio literalmente para dar um help nota 10!'} />
+        <CardClient photo={person1} name={'Celso Lopes'} text={'A Help veio literalmente para dar um help nota 10!'} />
+        <CardClient photo={person1} name={'Celso Lopes'} text={'A Help veio literalmente para dar um help nota 10!'} />
+        <CardClient photo={person1} name={'Celso Lopes'} text={'A Help veio literalmente para dar um help nota 10!'} />
+        <CardClient photo={person1} name={'Celso Lopes'} text={'A Help veio literalmente para dar um help nota 10!'} />
+        <CardClient photo={person1} name={'Celso Lopes'} text={'A Help veio literalmente para dar um help nota 10!'} />
+      </div>
+    </section>
+  );
+};
 
 export default ClientsSection;
