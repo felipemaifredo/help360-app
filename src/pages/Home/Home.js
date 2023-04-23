1b0371557bdd3a4323b2b63c78dd34243c201131
felipemaifredo <felipemaifredo@gmail.com>
2023-04-23 20:59:41 -0300

    att sec 5

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 //Imports
+import ScrollTo from "../../components/ScrollTo/ScrollTo";
 
 //Sections
 import Section1 from "./SectionsHome/Section1";
@@ -8,7 +9,6 @@ import Section4 from "./SectionsHome/Section4";
 import Section5 from "./SectionsHome/Section5";
 import Section6 from "./SectionsHome/Section6";
 import ClientsSection from "./SectionsHome/Section-Clients";
-import ScrollTo from "../../components/ScrollTo/ScrollTo";
 
 const Home = () => {
     return (
--- src/pages/Home/SectionsHome/Section5.js
+++ src/pages/Home/SectionsHome/Section5.js
@@ -6,7 +6,24 @@ import './style-section5.css';
 const Section5 = () => {
     return (
         <section id='sec5' className='section'>
-           
+           <div className='dash-box-sec5'>
+                <div className='box-top'>
+                    <div className='dash-card'></div>
+                    <div className='dash-card'></div>
+                    <div className='dash-card'></div>
+                    <div className='dash-card'></div>
+                </div>
+                <div className='box-bottom'>
+                    <div className='dash-card'></div>
+                    <div className='dash-card'></div>
+                    <div className='dash-card'></div>
+                    <div className='dash-card'></div>
+                </div>   
+           </div>
+           <div className='text-box-sec5'>
+                <h2>Com nossa dash, você terá total visão de seus atendimentos em aberto, encerrados, tickets, TME e TMA de sua equipe!</h2>
+                <a href='#'>Contratar</a>
+           </div>
         </section>
     )
 }
